9a66d5c73c78f419149e648759317a24b1b348ca
quinlin Hopperstad <quinlin.hopperstad@ucdenver.edu>
2023-04-15 17:25:25 -0600

    update dotenv

--- auth_jwt.js
+++ auth_jwt.js
@@ -3,8 +3,11 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+require('dotenv').config();
+const jwtOptions = {
+    jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt")
+    secretOrKey = process.env.SECRET_KEY
+}
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     var user = db.find(jwt_payload.id);
