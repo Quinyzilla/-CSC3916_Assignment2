f2cb2f8d839319283b50cf21b50a0cc518debe05
quinlin Hopperstad <quinlin.hopperstad@ucdenver.edu>
2023-04-19 09:39:32 -0600

    deleted test collection

--- server.js
+++ server.js
@@ -72,28 +72,6 @@ router.post('/signin', (req, res) => {
     }
 });
 
-router.route('/testcollection')
-    .delete(authController.isAuthenticated, (req, res) => {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-        }
-        var o = getJSONObjectForMovieRequirement(req);
-        res.json(o);
-    }
-    )
-    .put(authJwtController.isAuthenticated, (req, res) => {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-        }
-        var o = getJSONObjectForMovieRequirement(req);
-        res.json(o);
-    }
-    );
-
 router.route('/movies')
     .post(function (req, res){
       res.status(200).send({success: true, msg: 'movie saved'});
